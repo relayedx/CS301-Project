1165a74bb9c0f425f8d7274394675d9a56ca1d54
fosa. <obaseki321@gmail.com>
2025-11-24 18:27:00 -0800

    Remade main for Qt

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
-#include "Menu.h"
+#include "MainWindow.h"
+#include <QApplication>
 
-int main() {
-    Menu app;
-    app.run();
-    return 0;
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    
+    MainWindow window;
+    window.show();
+    
+    return app.exec();
 }
